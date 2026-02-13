398dc11518921a14bdbbcd18ca515829cc0850e4
Manish Kumar <kumarmanish.amr@gmail.com>
2026-02-13 17:43:32 +0530

    Update SocialLinks.jsx

--- src/components/ui/SocialLinks.jsx
+++ src/components/ui/SocialLinks.jsx
@@ -6,7 +6,7 @@ export default function SocialLinks({
 }) {
   return (
 <>
-	<Link href={link} className="relative group/link bg-black bg-opacity-50 h-[70px] flex items-center justify-center">
+	<Link href={link} target="_blank"  rel="noopener noreferrer" className="relative group/link bg-black bg-opacity-50 h-[70px] flex items-center justify-center">
 		<div className="absolute top-0 left-0 w-full h-0 group-[.hway-active]:h-full bg-black bg-opacity-50 border border-1 border-[#fff] transition-all duration-[.5s]"></div>
 		<div className="absolute top-0 left-0 w-full h-0 group-[.hway-active]:h-full bg-black border border-1 border-[#fff] transition-all duration-[.5s] delay-[.1s]"></div>
 		<div className="absolute top-0 left-0 w-full h-0 group-hover/link:h-full bg-[#049fe1] opacity-50 transition-all duration-[.5s] delay-[.25s] group-hover:delay-[0s]"></div>
